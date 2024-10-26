8faa102b94be90e4f4b60947d65294360c2f77ce
Jose Pedro Silva <josepedro88@live.com.pt>
2024-10-26 14:53:10 +0200

    Add token authentication middleware for protected routes

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+// Token authentication middleware
+const authenticateTokenMiddleware = (req, res, next) => {
+    const token = req.header('Authorization')?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).send('Access denied');
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).send('Invalid token');
+        }
+
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = authenticateTokenMiddleware;
